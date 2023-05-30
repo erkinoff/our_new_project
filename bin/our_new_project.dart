16a988a27ed09a8df8124e9c6d363ce6fb2fa760
erkinoff <murodjonerkinov2005@gmail.com>
2023-05-30 23:50:48 +0500

    .

--- bin/our_new_project.dart
+++ bin/our_new_project.dart
@@ -2,4 +2,6 @@ void main(List<String> args) {
   print("Hello form another side of network!✋");
   print("This is now a new telegram for us😂"); //erkinoff
   print("Good idea👍");
+  print(
+      "Vazifa nimedi o'zi? ulanishshi tekshirse bo'ldimi? yoki ubu proyekt qivorishmi");
 }
